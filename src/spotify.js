587c9d070fc40a56ad77732a5cf106d828dd03d6
sanaeas <sanaassim1@gmail.com>
2022-12-10 09:17:05 +0100

    Initialize the project & create the login page, use Spotify API for authentication

---
+++ src/spotify.js
@@ -0,0 +1,31 @@
+// Spotify API's documentation
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "cf67696082824ca79a472f0618b7ed98";
+const redirectUri = "http://localhost:3000/";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+
+            return initial;
+        }, {});
+} 
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
