aca3188cc92826b9fac0dd582f4ab11e822c8dab
sanaeas <sanaassim1@gmail.com>
2022-12-18 12:16:25 +0100

    create the different components of the player page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Login from './components/Login';
-import { getTokenFromUrl } from './spotify';
-import SpotifyWebApi from 'spotify-web-api-js';
-import Player from './components/Player';
-import { useStateValue } from './StateProvider';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/Player";
+import { useStateValue } from "./StateProvider";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [{}, dispatch] = useStateValue();
+  const [{ token }, dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -19,30 +18,25 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
+      spotify.getMe().then((user) => {
         dispatch({
-          type: 'SET_USER',
-          user: user
-        })
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
-
-    console.log(token)
   }, []);
 
   return (
     <div className="app">
-      {
-        token ? (
-          <Player />
-        ) : (
-          <Login />
-        )
-      }
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
+    item: null,
+    // REMOVE after developing
+    token: 'BQCiFaH7SAHLqM1-Vzg0Z94CYjcbSDHPW-oBgJ6eNsdEfabqtU…8K73sLoJwVd2jfaj_N9xOo1AqreY0fol5fX96lNVjbgsRYi9s',
 };
 
 const reducer = (state, action) => {
@@ -13,6 +15,11 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 user: action.user,
+            };
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token,
             };
         default:
             return state;
