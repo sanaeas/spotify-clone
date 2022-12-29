a64683e7cbc2337191ff6ea9a05eac5ff895d902
sanaeas <sanaassim1@gmail.com>
2022-12-29 09:22:09 +0100

    Finish the sidebar, and get user playlists from Spotify

---
+++ src/components/SidebarOption.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SidebarOption.css';
+
+function SidebarOption({ title, Icon }) {
+  return (
+    <div className='sidebar__option'>
+        { Icon && <Icon className='sidebar__option--icon' /> }
+        { Icon ? <h4>{title}</h4> : <p>{title}</p> }
+    </div>
+  )
+}
+
+export default SidebarOption
